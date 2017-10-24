1f1e96572863fc4c964a8b87640b6bce9f309e7e
Brian Simoni <simonibc@vcu.edu>
2017-10-23 20:28:53 -0400

    Add Cred file, improve filters

---
+++ src/main/java/Creds.java
@@ -0,0 +1,7 @@
+/**
+ * Created by brian on 10/19/17.
+ */
+public class Creds {
+	public static final String USERNAME = "";
+	public static final String PASSWORD = "";
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,20 +5,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.*;
+import java.util.NoSuchElementException;
 
 /**
  * Created by brian on 10/14/17.
  */
 public class Main {
 
-	public static final String USERNAME = "";
-	public static final String PASSWORD = "";
-
 
 	public static void main(String[] args) {
-		// declaration and instantiation of objects/variables
+
 		System.setProperty("webdriver.firefox.marionette", "/home/brian/youtubeadvertiser/geckodriver");
 		WebDriver driver = new FirefoxDriver();
 		//comment the above 2 lines and uncomment below 2 lines to use Chrome
@@ -31,34 +31,35 @@ public class Main {
 		// launch Fire fox and direct it to the Base URL
 		driver.get(baseUrl);
 
-        driver.findElement(By.id("identifierId")).sendKeys(USERNAME);
+		driver.findElement(By.id("identifierId")).sendKeys(Creds.USERNAME);
 
-		driver.findElement(By.id("identifierNext")).click();
+        driver.findElement(By.id("identifierNext")).click();
 
 		WebDriverWait wait = new WebDriverWait(driver, 8);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
 
-		driver.findElement(By.name("password")).sendKeys(PASSWORD);
+		driver.findElement(By.name("password")).sendKeys(Creds.PASSWORD);
 
 		driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
 
 		wait.until(ExpectedConditions.titleIs("My Account"));
 
-		Set<String> visited = new HashSet<String>();
+		Set<String> visited = getVisitedNodesFromLogs();
 		Queue<String> relatedVideos = new LinkedList<String>();
 
 		// Start node
 		try {
-			commentOnVideo("https://www.youtube.com/watch?v=VWUh4z57v9o&t=3s", relatedVideos, driver);
+			commentOnVideo("https://www.youtube.com/watch?v=Aqioba4qHZk", relatedVideos, driver);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 
 
-		while(visited.size() < 1000) {
+		while(visited.size() < 10000 && relatedVideos.size() > 0) {
 			String nextVideo = relatedVideos.poll();
 			if (visited.contains(nextVideo)) {
+				System.out.println(nextVideo + " was in the visited log");
 				continue;
 			}
 			try {
@@ -68,7 +69,6 @@ public class Main {
 				writeLog("Failed to comment on video: " + nextVideo);
 			}
 			visited.add(nextVideo);
-			writeLog(nextVideo);
 			System.out.println("related video queue size " + relatedVideos.size());
 		}
 
@@ -100,7 +100,13 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		driver.findElement(By.cssSelector("#submit-button a[is='yt-endpoint'")).click();
+		boolean pubgVideoCategory = isPubgVideo(driver);
+
+		if(pubgVideoCategory && !alreadyCommented(driver)) {
+			// Click the submit button
+			driver.findElement(By.cssSelector("#submit-button a[is='yt-endpoint'")).click();
+			writeLog(videoID);
+		}
 
 		try {
 			Thread.sleep(2000);
@@ -108,11 +114,18 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		List<WebElement> hrefList =  driver.findElements(By.cssSelector(".yt-simple-endpoint.style-scope.ytd-compact-video-renderer"));
 
-		for (WebElement video : hrefList) {
-			String related = video.getAttribute("href");
-			relatedVideos.add(related);
+		// Only add the list of related videos, if the current video is a PUBG related video.
+		// No need to explore unrelated branches of the tree.
+		// Only targeting PUBG players.
+		if (pubgVideoCategory) {
+			thumbsUp(driver);
+			List<WebElement> hrefList =  driver.findElements(By.cssSelector(".yt-simple-endpoint.style-scope.ytd-compact-video-renderer"));
+
+			for (WebElement video : hrefList) {
+				String related = video.getAttribute("href");
+				relatedVideos.add(related);
+			}
 		}
 
 	}
@@ -127,5 +140,61 @@ public class Main {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	static boolean isPubgVideo(WebDriver driver) {
+
+		WebElement element;
+		try {
+			 element = driver.findElement(By.cssSelector("a[href='https://gaming.youtube.com/game/UC_qMbhHGOBF_dWCmXd5x4OQ']"));
+		} catch(org.openqa.selenium.NoSuchElementException e) {
+			System.out.println("This is not a PUBG related video");
+			return false;
+		}
+
+		if(element == null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	static boolean alreadyCommented(WebDriver driver) {
+		List<WebElement> elements = driver.findElements(By.cssSelector("#content-text"));
+		for(WebElement element : elements) {
+			if (element.getText().contains("Wow this is almost as great as pubgswag.com")) {
+				System.out.println("Already commented on this video");
+				return true;
+			}
+		}
+		return false;
+	}
+
+	static void thumbsUp(WebDriver driver) {
+		// TODO: make sure we are upvoting our own comment
+//		List<WebElement> elements = driver.findElements(By.cssSelector("#comment"));
+//		for(WebElement element : elements) {
+//			if (element.findElement(By.cssSelector("#content-text")).getText().contains("Wow this is almost as great as pubgswag.com")) {
+//				WebElement thumbUp = element.findElement(By.cssSelector("#like-button"));
+//				thumbUp.click();
+//			}
+//		}
+
+		List<WebElement> thumbUpList = driver.findElements(By.cssSelector("#like-button"));
+				thumbUpList.get(0).click();
+	}
+
+	static Set<String> getVisitedNodesFromLogs() {
+		Set<String> visited = new HashSet<String>();
+		Scanner fileScanner;
+		try {
+			fileScanner = new Scanner(new File("/home/brian/youtubeadvertiser/visited.txt"));
+			while (fileScanner.hasNext()){
+				visited.add(fileScanner.next());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return visited;
 	}
 }
